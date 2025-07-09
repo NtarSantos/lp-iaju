ca630abfba311b2bb76f9a31ce658a07321b4554
Natanael Santos <95532185+NtarSantos@users.noreply.github.com>
2025-07-08 22:30:54 -0300

    Update Header.tsx
    logo off

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,6 @@ const Header = () => {
   return (
     <header className="bg-light-yellow py-6 border-b border-yellow-200">
       <div className="container mx-auto px-6">
-        <div className="flex justify-center">
-          <img 
-            src="/lovable-uploads/ec10155c-62a0-464f-a6f5-c1091129a71a.png" 
-            alt="iAju - Inteligência Artificial" 
-            className="h-20 w-auto"
-          />
-        </div>
       </div>
     </header>
   );
