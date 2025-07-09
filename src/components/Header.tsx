f9343d74d68e873c6472d9ec00e7891fa4c0fa55
Natanael Santos <95532185+NtarSantos@users.noreply.github.com>
2025-07-08 22:32:53 -0300

    Update Header.tsx
    hheader off

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,1 @@
 
-const Header = () => {
-  return (
-    <header className="bg-light-yellow py-6 border-b border-yellow-200">
-      <div className="container mx-auto px-6">
-      </div>
-    </header>
-  );
-};
-
-export default Header;
